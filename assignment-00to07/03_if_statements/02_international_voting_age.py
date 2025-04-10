7848e1429caa0ef05c989dcd169ff665256a2f4c
Muhammad Sufyan <muhammadsufyan4678@gmail.com>
2025-04-10 18:29:03 +0500

    assignment-4

---
+++ assignment-00to07/03_if_statements/02_international_voting_age.py
@@ -0,0 +1,25 @@
+peturksbouipo: int = 16
+stanlau: int = 25
+mayengua: int = 48
+age: int = int(input("How old are you? "))
+
+def main():
+    
+
+    if age >= peturksbouipo:
+        print(f"Your age is {age}.you are eligible to vote in Peturksbouipo.")
+    else:
+        print(f"Your age is {age}.you are not eligible to vote in Peturksbouipo.") 
+
+    if age >= stanlau:
+        print(f"Your age is {age}.you are eligible to vote in Stanlau.")
+    else:
+        print(f"Your age is {age}.you are not eligible to vote in Stanlau.") 
+
+    if age >= mayengua:
+        print(f"Your age is {age}.you are eligible to vote in Mayengua.")
+    else:
+        print(f"Your age is {age}.you are not eligible to vote in Mayengua.") 
+
+if __name__ == "__main__":
+    main()
