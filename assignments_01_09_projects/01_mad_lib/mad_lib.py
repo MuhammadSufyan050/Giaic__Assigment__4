f9ca6819cf610bc5afc96a2106b808bda115664f
Muhammad Sufyan <muhammadsufyan4678@gmail.com>
2025-04-14 01:09:20 +0500

    assigment-4

---
+++ assignments_01_09_projects/01_mad_lib/mad_lib.py
@@ -0,0 +1,10 @@
+name = input("Enter the boy name: ")
+programming_language = input("Enter a programming language: (e.g:python , next js , typescript , html): ")
+mentor = input("Enter the mentor name: ")
+
+print(f"\nHere is my little story based on mad libs game!")
+print(f"one's upon a time, there was a boy named {name}.")
+print(f"{name} was very curious and always want to learn new things")
+print(f"One day, he decided to learn {programming_language} language at governor house!")
+print(f"Luckily {name} found a great mentor name as {mentor} who was an expert in {programming_language}.")
+print("And so, his journey continues...!")
